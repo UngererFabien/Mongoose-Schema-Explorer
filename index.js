c3bed5f70b3ff08ad2bade02e273732f00baab54
Julien Leray <leray.jul@gmail.com>
2018-04-25 16:33:47 +0200

    getting schema from mongoose

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-exports = function(mongoose) {
-	console.log("SCHEMAS: ", mongoose.modelSchemas);
+const _ = require("lodash");
+
+module.exports = function(mongoose) {
+	let schemas = mongoose.modelSchemas;
+	let schemasKeys = _.keys(schemas);
+	console.log("schemasKeys: ", schemasKeys);
+
+	console.log(
+		"SCHEMA building: ",
+		JSON.stringify(schemas.building, null, "\t")
+	);
+
+	schemasKeys.forEach(e => {
+		let schema = schemas[e].obj;
+		//console.log(schema);
+	});
 };
