ac19077140fa39fadfafc7090d087b06b7207996
Julien Leray <leray.jul@gmail.com>
2018-04-27 11:12:57 +0200

    wip

--- index.js
+++ index.js
@@ -3,15 +3,39 @@ const _ = require("lodash");
 module.exports = function(mongoose) {
 	let schemas = mongoose.modelSchemas;
 	let schemasKeys = _.keys(schemas);
-	console.log("schemasKeys: ", schemasKeys);
+	//console.log("schemasKeys: ", schemasKeys);
 
 	console.log(
 		"SCHEMA building: ",
-		JSON.stringify(schemas.building, null, "\t")
+		JSON.stringify(schemas.building.tree, null, "\t")
 	);
+
+	let results = [
+		{
+			schema: "building"
+		}
+	];
 
 	schemasKeys.forEach(e => {
 		let schema = schemas[e].obj;
-		//console.log(schema);
+
+		if (Array.isArray(schema)) {
+			root = root[0];
+		}
+
+		console.log(schema);
 	});
 };
+
+function exploreLevel(root) {
+	if (Array.isArray(root)) {
+		root = root[0];
+	}
+
+	_.forOwn(root, (val, key) => {});
+}
+
+//{} || [{}] || [null] || field
+function isReference(val) {
+	if (val.ref) return val.ref;
+}
